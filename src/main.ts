014b88d073ce097b198bc5d07f4ec7c43bae45f9
lowbrain <83059975+lowbrain@users.noreply.github.com>
2023-02-03 23:36:35 +0900

    よくわからないがマニュアル通りに設定

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,17 @@ import router from "./router";
 
 import "./assets/main.css";
 
+import { registerSW } from "virtual:pwa-register";
+const updateSW = registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() {},
+});
+
 // Vuetify
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
-
 const vuetify = createVuetify({
   components,
   directives,

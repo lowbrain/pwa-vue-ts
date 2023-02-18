6efbe92682ada6328dc7d8845993b868977548a8
lowbrain <83059975+lowbrain@users.noreply.github.com>
2023-02-18 12:16:10 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
-      injectRegister: "script",
+      injectRegister: "inline",
       devOptions: { enabled: true },
       includeAssets: ["img/**/*"],
       workbox: { globPatterns: ["**/*.{js,css,html,ico,png,jpg}"] },

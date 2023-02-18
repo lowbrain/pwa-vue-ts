335f1e45474a8e7b7a908c160b234fe141fb0b8b
lowbrain <83059975+lowbrain@users.noreply.github.com>
2023-02-18 14:49:26 +0900

    NotFoundのページを追加

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import MenuView from "../views/MenuView.vue";
 import LoginView from "../views/LoginView.vue";
+import NotFoundView from "../views/NotFoundView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -27,6 +28,11 @@ const router = createRouter({
         requiresInstall: true,
       },
       component: LoginView,
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "notfound",
+      component: NotFoundView,
     },
   ],
 });
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+console.log("NotFoundView");
+</script>
+
+<template>
+  <span>Not Found</span>
+</template>

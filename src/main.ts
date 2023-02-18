763572a3775b53f95e7736fca2bdc283251fe46b
lowbrain <83059975+lowbrain@users.noreply.github.com>
2023-02-18 15:36:02 +0900

    IconをSVGに変更

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,20 @@ import router from "./router";
 // Vuetify
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
-import "@mdi/font/css/materialdesignicons.css";
+import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
 });
 
 const app = createApp(App);
--- src/views/MenuView.vue
+++ src/views/MenuView.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
+import { mdiDotsVertical } from "@mdi/js";
 import ReloadPrompt from "../components/ReloadPrompt.vue";
-
+const mdiDotsVIcon = mdiDotsVertical;
 console.log("MenuView");
 </script>
 
 <template>
   <v-app-bar color="primary">
     <v-app-bar-title>PWA-VUE-TS</v-app-bar-title>
-    <v-btn icon="mdi-dots-vertical"></v-btn>
+    <v-btn :icon="mdiDotsVIcon"></v-btn>
   </v-app-bar>
   <v-main> menu </v-main>
   <ReloadPrompt />

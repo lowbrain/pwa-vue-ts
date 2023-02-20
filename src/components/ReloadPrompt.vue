86d197fda082c59d110b6e04a5032f416017582a
lowbrain <83059975+lowbrain@users.noreply.github.com>
2023-02-20 22:25:02 +0900

    おためし

--- src/components/ReloadPrompt.vue
+++ src/components/ReloadPrompt.vue
@@ -5,10 +5,11 @@ import { useRegisterSW } from "virtual:pwa-register/vue";
 const intervalMS: number = 60 * 1000;
 const { offlineReady, needRefresh, updateServiceWorker } = useRegisterSW({
   onRegisteredSW(swUrl, r) {
-    r &&
-      setInterval(async () => {
-        await r.update();
-      }, intervalMS);
+    if (r) {
+      const update = async () => await r.update();
+      update();
+      setInterval(update, intervalMS);
+    }
   },
 });
 
@@ -29,10 +30,8 @@ console.log("ReloadPrompt");
 
 <template>
   <v-snackbar v-model="needRefresh" location="bottom right" timeout="-1">
-    <p>
-      新しいコンテンツが適用可能です。今すぐアプリを再起動し適用しますか？
-      キャンセルした場合は、次回アプリ起動時などに自動適用されます。
-    </p>
+    <p>新しいコンテンツが適用可能です。今すぐアプリを再起動し適用しますか？</p>
+    <p>（キャンセルした場合は、次回アプリ起動時などに自動適用されます）</p>
     <div class="d-flex justify-end">
       <v-btn color="pink" variant="text" @click="updateServiceWorker()">今すぐ適用</v-btn>
       <v-btn color="pink" variant="text" @click="close">キャンセル</v-btn>

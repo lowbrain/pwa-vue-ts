0985defdbaa6e5b5e36099482b234ad32009ba1d
lowbrain <83059975+lowbrain@users.noreply.github.com>
2023-02-23 10:36:33 +0900

    自URLを完全パスで設定

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -5,8 +5,10 @@ import { checkServerStatus } from "@/modules/check-status";
 import AppLogo from "@/components/AppLogo.vue";
 import ProgressOverlay from "@/components/ProgressOverlay.vue";
 
+const returnUrl = `${location.protocol}//${location.host}${import.meta.env.BASE_URL}login`;
+
 // eslint-disable-next-line prettier/prettier
-const loginURL: string = "https://first-server.azurewebsites.net/login.jsp?popup_url=https%3A%2F%2Fsecond-server.azurewebsites.net%2Fpopup.jsp%3Fjump_url%3Dhttps%3A%2F%2Fsecond-server.azurewebsites.net%2Findex.jsp&auth_url=https%3A%2F%2Fsecond-server.azurewebsites.net%2Fauth.jsp&return_url=https%3A%2F%2Ffirst-server.azurewebsites.net%2Fredirect.jsp&redirect_url=" + encodeURIComponent(window.location.href);
+const loginURL: string = "https://first-server.azurewebsites.net/login.jsp?popup_url=https%3A%2F%2Fsecond-server.azurewebsites.net%2Fpopup.jsp%3Fjump_url%3Dhttps%3A%2F%2Fsecond-server.azurewebsites.net%2Findex.jsp&auth_url=https%3A%2F%2Fsecond-server.azurewebsites.net%2Fauth.jsp&return_url=https%3A%2F%2Ffirst-server.azurewebsites.net%2Fredirect.jsp&redirect_url=" + returnUrl;
 
 const errMsg: any = ref(null);
 

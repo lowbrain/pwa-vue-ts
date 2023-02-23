3a389e242b570e78008c98e3ac296bd441975d95
lowbrain <83059975+lowbrain@users.noreply.github.com>
2023-02-23 21:51:59 +0900

    Update appversion.ts

--- src/modules/appversion.ts
+++ src/modules/appversion.ts
@@ -1,6 +1,6 @@
 const MAJOR_V: string = "0";
 const MINOR_V: string = "1";
-const PATCH_V: string = "0";
+const PATCH_V: string = "1";
 const appVersion: string = `${MAJOR_V}.${MINOR_V}.${PATCH_V}`;
 
 export default appVersion;

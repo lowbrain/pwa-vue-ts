64e789a617a28fbbc37856d604c3884d0a069fbf
LowBrain <83059975+lowbrain@users.noreply.github.com>
2023-02-24 19:26:42 +0900

    更新メッセージを１行に

--- src/components/ReloadPrompt.vue
+++ src/components/ReloadPrompt.vue
@@ -21,8 +21,10 @@ console.log("ReloadPrompt");
 
 <template>
   <v-snackbar v-model="needRefresh" location="bottom right" timeout="-1">
-    <p>新しいコンテンツが適用可能です。今すぐアプリを再起動し適用しますか？</p>
-    <p>（キャンセルした場合は、次回アプリ起動時などに自動適用されます）</p>
+    <p>
+      新しいコンテンツが適用可能です。今すぐアプリを再起動し適用しますか？
+      （キャンセルした場合は、次回アプリ起動時などに自動適用されます）
+    </p>
     <div class="d-flex justify-end">
       <v-btn color="pink" variant="text" @click="updateServiceWorker()">今すぐ適用</v-btn>
       <v-btn color="pink" variant="text" @click="close">キャンセル</v-btn>
